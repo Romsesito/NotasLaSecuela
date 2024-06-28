d786a8bb7c3b50b4583667404c55c1d34f5a014c
Romsesito <graphicsrom@gmail.com>
2024-06-28 10:48:38 -0500

    Mejoras en la interfaz, y links de redireccionamiento a redes.

--- Views/AboutPage.xaml.cs
+++ Views/AboutPage.xaml.cs
@@ -6,4 +6,36 @@ public partial class AboutPage : ContentPage
     {
         InitializeComponent();
     }
+
+
+
+    private async void github(object sender, EventArgs e)
+    {
+        // URL que deseas abrir
+        string url = "https://github.com/Romsesito";
+
+        // Verifica si el URL puede ser abierto y luego lo abre
+        if (await Launcher.CanOpenAsync(url))
+        {
+            await Launcher.OpenAsync(url);
+        }
+    }
+
+    private async void yutu(object sender, EventArgs e)
+    {
+        string url = "https://www.youtube.com/channel/UCe-tRcEe9RxDMvyRfXaZU9Q";
+        if (await Launcher.CanOpenAsync(url))
+        {
+            await Launcher.OpenAsync(url);
+        }
+    }
+
+    private async void steam(object sender, EventArgs e)
+    {
+        string url = "https://steamcommunity.com/profiles/76561198274277930/";
+        if (await Launcher.CanOpenAsync(url))
+        {
+            await Launcher.OpenAsync(url);
+        }
+    }
 }
